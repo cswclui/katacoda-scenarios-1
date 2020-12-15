3e150a8ac57eb0b01195329ea8a71153ea4d94eb
Bob Reselman <reselbob@gmail.com>
2020-12-14 19:39:48 -0800

    building out 8

--- 12factor-008/foreground.sh
+++ 12factor-008/foreground.sh
@@ -8,5 +8,32 @@ sudo apt install tree -y && clear
 
 docker run -d --network="host" -p 5000:5000 --restart=always --name registry registry:2 && clear
 
+#Add KinD
+
+curl -LO "https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl"
+
+sudo chmod +x ./kubectl
+
+sudo mv ./kubectl /usr/local/bin/kubectl
+
+curl -Lo ./kind https://kind.sigs.k8s.io/dl/v0.9.0/kind-linux-amd64
+
+sudo chmod +x ./kind
+
+sudo mv ./kind /usr/local/bin/kind
+
+cat > kind-config.yaml << "EOF"
+kind: Cluster
+apiVersion: kind.x-k8s.io/v1alpha4
+nodes:
+  - role: control-plane
+  - role: worker
+  - role: worker
+EOF
+
+kind create cluster --config kind-config.yaml && clear
+
+kubectl get nodes
+
 echo "You're good to go!"
 
