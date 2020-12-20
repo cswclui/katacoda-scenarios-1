cdd78b3cb751b031c8920e4b747dff531c368bb5
Bob Reselman <reselbob@gmail.com>
2020-12-20 12:05:51 -0800

    fixing foreground bug

--- 12factor-010/foreground.sh
+++ 12factor-010/foreground.sh
@@ -8,6 +8,8 @@ sudo apt install tree -y && clear
 
 sudo docker run -d --network="host" -p 5000:5000 --restart=always --name registry registry:2 && clear
 
+gpg --keyserver keyserver.ubuntu.com --recv-keys 16126D3A3E5C1192 
+
 echo "installing Jenkins"
 
 wget -q -O - http://pkg.jenkins-ci.org/debian/jenkins-ci.org.key | sudo apt-key add -
