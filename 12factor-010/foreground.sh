427cc1d9e287253a28df127adfa8b79851c7b99f
Bob Reselman <reselbob@gmail.com>
2020-12-20 11:14:03 -0800

    fixing foreground bug

--- 12factor-010/foreground.sh
+++ 12factor-010/foreground.sh
@@ -2,11 +2,11 @@ git config --global pager.branch false
 
 sudo apt update -y
 
-sudo apt-get install jq -y
+sudo apt-get install jq -y && clear
 
-sudo apt install tree -y
+sudo apt install tree -y && clear
 
-sudo docker run -d --network="host" -p 5000:5000 --restart=always --name registry registry:2
+sudo docker run -d --network="host" -p 5000:5000 --restart=always --name registry registry:2 && clear
 
 echo "You're good to go!"
 
