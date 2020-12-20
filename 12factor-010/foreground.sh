5ac76a67508304a73e211d3b310615f4d0d10c28
Bob Reselman <reselbob@gmail.com>
2020-12-20 13:27:11 -0800

    fixing foreground bug

--- 12factor-010/foreground.sh
+++ 12factor-010/foreground.sh
@@ -8,35 +8,5 @@ sudo apt install tree -y && clear
 
 sudo docker run -d --network="host" -p 5000:5000 --restart=always --name registry registry:2 && clear
 
-echo "installing Jenkins"
-
-wget -qO - https://pkg.jenkins.io/debian-stable/jenkins.io.key | apt-key add -
-
-sudo sh -c 'echo deb http://pkg.jenkins-ci.org/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list'
-
-sudo apt update -y
-
-sudo apt install jenkins -y
-
-echo "usermod -aG sudo jenkins"
-
-sudo groupadd docker
-
-sudo usermod -aG docker $USER
-
-sudo chmod 777 /var/run/docker.sock
-
-sudo systemctl start jenkins
-
-# sudo ufw allow 8080
-
-# sudo ufw status
-
-# sudo mount -o remount,exec /var
-
-
-echo "Installed Jenkins"
-
-
 echo "You're good to go!"
 
